216f2d645428ad1dcd5bda153ae6ee41cb0cac13
PVigar88 <Pvigar@gmail.com>
2021-06-16 22:32:56 -0400

    fixing more issues

--- watermyplants/src/index.js
+++ watermyplants/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import reducer from "reducers";
+import reducer from "./reducers";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import logger from "redux-logger";
