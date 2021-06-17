2f68e39bb5a2e8edaec27ab75c4d55c598d573b2
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-06-16 20:55:04 -0700

    added input fields for plant nickname, name, species, and water frequency

--- watermyplants/src/App.js
+++ watermyplants/src/App.js
@@ -1,22 +1,15 @@
 import "./App.css";
 import React from "react";
+import Plant from './components/plantComponent'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <Plant />
         <img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
--- watermyplants/src/components/plantComponent.js
+++ watermyplants/src/components/plantComponent.js
@@ -15,24 +15,62 @@ const onSubmit = evt => {
     submit()
 }
 
-const onChange = evt = {
-    const { name, value, checked, type } = evt.target
-    change(name, valueToUse)
-}
+// const onChange = evt = {
+//     const { name, value, checked, type } = evt.target
+//     change(name, valueToUse)
+// }
 return (
     <form className='form container' onSubmit={onSubmit}>
         <div className='form-group submit'>
-            <h2>Add a Plant</h2>
-            <button disabled={disabled}>add plant</button>
+            <h2>Water My Plants</h2>
+            
         </div>
 
         <div className='form-group inputs'>
-            <h4>Plant Info</h4>
+            <h4>Add a plant </h4>
+            <label>plant's nickname
+                <input
+                // value={values.plantNickname}
+                // onChange={onChange}
+                name='plantNickname'
+                type='text'
+                />
+            </label>
 
-            <label>
-                
+            <label>plant's name
+                <input
+                // value={values.plantName}
+                // onChange={onChange}
+                name='plantName'
+                type='text'
+                />
+            </label>
+
+            <label>species
+                <input
+                // value={values.plantName}
+                // onChange={onChange}
+                name='species'
+                type='text'
+                />
+            </label>
+
+            <label>watering frequency
+                <input
+                // value={values.plantName}
+                // onChange={onChange}
+                name='wateringFrequency'
+                type='text'
+                />
             </label>
         </div>
+        
+        <h6>upload image</h6>
+
+        <button disabled={disabled}>add plant</button>
+        <button>cancel</button>
+
+        
     </form>
 
 )
