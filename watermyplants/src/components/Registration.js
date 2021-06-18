f37c9b7e2fe716a09dea5879b12ce80caf1da2ec
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-06-17 21:53:17 -0400

    created registration component

---
+++ watermyplants/src/components/Registration.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const Registration = () => {
+    const [form, setForm] = useState({
+        username: "",
+        "phone number": "",
+        password: "",
+        "repeat password": ""
+    });
+
+    return (
+        <>
+            <form>
+                <label>
+                    <input  
+                        name="username"
+                        value={form.username}
+                    />
+                </label>
+                <label>
+                    <input 
+                        name="phone number"
+                        value={form["phone number"]}
+                    />
+                </label>
+                <label>
+                    <input 
+                        name="password"
+                        value={form.password}
+                    />
+                </label>
+                <label>
+                    <input 
+                        name="repeat password"
+                        value={form["repeat password"]}
+                    />
+                </label>
+            </form>
+        </>
+    )
+}
