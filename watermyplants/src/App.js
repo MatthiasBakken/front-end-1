c37f21be92c0dd388d70123d0e70e7a05f6daed6
PVigar88 <Pvigar@gmail.com>
2021-06-19 16:03:48 -0400

    cleaned up app, resolved compile errors, still nee to add some pathways

--- watermyplants/src/App.js
+++ watermyplants/src/App.js
@@ -1,48 +1,25 @@
 import "./App.css";
 import React from "react";
 import Login from "./components/Login";
-<<<<<<< HEAD
 import Plant from "./components/plantComponent";
 import Registration from "./components/Registration";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
-=======
-<<<<<<< HEAD
-import userProfile from "./components/userProfile";
-=======
-import Registration from "./components/Registration";
->>>>>>> main
-
-import Plant from './components/plantComponentForm.js'
 import PlantHelper from "./components/plantComponentHelper";
->>>>>>> main
+import userProfile from "./components/userProfile";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <Header />
       </header>
       <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/adduser" component={Registration} />
         <Route path="/addplant" component={Plant} />
+        <Route path="/user" component={userProfile} />
       </Switch>
-=======
-        <Login />
-<<<<<<< HEAD
-<<<<<<< HEAD
-        
-=======
-        <userProfile />
-
->>>>>>> 4f9f90bdeb3950e5ddb0bdf63106e5df12f816dc
-=======
-        <Registration />
->>>>>>> main
-      </header>
->>>>>>> main
     </div>
   );
 }
--- watermyplants/src/components/userProfile.js
+++ watermyplants/src/components/userProfile.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-const userProfileComp = () => { 
-    const { details, setDetails } = useState(null)
+const userProfileComp = () => {
+  const { details, setDetails } = useState(null);
 
-    return (
-        <div className='profileContainer'>
-            <h2>User Profile</h2>
-            {
-                <>
-                    <a>User name</a>
-                    <a>{details.user.username}</a>
-                    <a>Password</a>
-                    <a>{details.user.password}</a>
-                    <a>Phone number</a>
-                    <a>{details.user.phoneNumber}</a>
-                </>
-            }
-            <button>complete</button>
-            <button>cancel</button> 
+  return (
+    <div className="profileContainer">
+      <h2>User Profile</h2>
+      {
+        <div>
+          <a>User name</a>
+          <a>{details.user.username}</a>
+          <a>Password</a>
+          <a>{details.user.password}</a>
+          <a>Phone number</a>
+          <a>{details.user.phoneNumber}</a>
         </div>
-    )
-}
+      }
+      <button>complete</button>
+      <button>cancel</button>
+    </div>
+  );
+};
 
 export default userProfileComp;
