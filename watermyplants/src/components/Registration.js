cfc14b3d519309ef103acd5bc8a3fd73e9b50b95
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-06-18 22:42:31 -0400

    added styling to the registration component

--- watermyplants/src/components/Registration.js
+++ watermyplants/src/components/Registration.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const Registration = () => {
     const [form, setForm] = useState({
@@ -8,40 +9,59 @@ const Registration = () => {
         "repeat password": ""
     });
 
+    const StyledForm = styled.form`
+        width: 500px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+    `;
+
+    const StyledButton = styled.button`
+        width: 30%;
+        height: 3rem;
+        margin-top: 3rem;
+        margin-left: 37%;
+    `;
+
     return (
         <div>
-            <p>Welcome to Water My Plants! <br /> Create your account here!</p>
-            <form>
-                <label>
-                    Username
-                    <input  
-                        name="username"
-                        value={form.username}
-                    />
-                </label>
-                <label>
-                    Phone Number
-                    <input 
-                        name="phone number"
-                        value={form["phone number"]}
-                    />
-                </label>
-                <label>
-                    Password 
-                    <input 
-                        name="password"
-                        value={form.password}
-                    />
-                </label>
-                <label>
-                    Repeat Password
-                    <input 
-                        name="repeat password"
-                        value={form["repeat password"]}
-                    />
-                </label>
-                <button>Complete</button>
-            </form>
+            <p> 
+                <strong>Welcome to Water My Plants!</strong> 
+                <br /> 
+                <i>Create your account here!</i> 
+            </p>
+            <StyledForm>
+                    <label>
+                        Username:
+                        <input  
+                            name="username"
+                            value={form.username}
+                        />
+                    </label>
+                    <label>
+                        Phone Number:
+                        <input 
+                            name="phone number"
+                            value={form["phone number"]}
+                        />
+                    </label>
+                    <label>
+                        Password: 
+                        <input 
+                            name="password"
+                            value={form.password}
+                        />
+                    </label>
+                    <label>
+                        Repeat Password:
+                        <input 
+                            name="repeat password"
+                            value={form["repeat password"]}
+                        />
+                    </label>
+                    <StyledButton>Complete</StyledButton>
+            </StyledForm>
+           
         </div>
     )
 }
