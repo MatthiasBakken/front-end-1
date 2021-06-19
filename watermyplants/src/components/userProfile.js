d0285dddcde4228ad23b3d34505838f868fe5741
heliokles <heliokles@github.com>
2021-06-18 19:44:39 -0700

    resolved merged conflict 2.0

--- watermyplants/src/components/userProfile.js
+++ watermyplants/src/components/userProfile.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 
-<<<<<<< HEAD
 const userProfileComp = () => { 
     const { details, setDetails } = useState(null)
 
@@ -23,33 +22,4 @@ const userProfileComp = () => {
     )
 }
 
-=======
-const userProfileComp = () => {
-    return (
-    <div>
-        <div>
-            <div className='userName'>
-                <label>User name</label>
-                <a>`${username}`</a>
-            </div>
-            <div className='password'>
-                <label>password</label>
-                <a>`${password}`</a>
-            </div>
-            <div className='phoneNumber'>
-                <label>Phone number</label>
-                <a>`${phoneNumber}`</a>
-            </div>
-        </div>
-        <div>
-            <button>complete</button>
-            <button>cancel</button>
-        </div>
-    </div>
-    )
-}
-
-
-
->>>>>>> 4f9f90bdeb3950e5ddb0bdf63106e5df12f816dc
 export default userProfileComp;
