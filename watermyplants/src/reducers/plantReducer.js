749727861b2826fb821df13cc46c6f9a33b53065
PVigar88 <Pvigar@gmail.com>
2021-06-19 22:15:03 -0400

    added plantReducer structure, need to check function

--- watermyplants/src/reducers/plantReducer.js
+++ watermyplants/src/reducers/plantReducer.js
@@ -1,19 +1,76 @@
-import {} from "../actions";
+import {
+  LOADING_PLANTS,
+  PLANT_SUCCESS,
+  PLANT_FAIL,
+  EDIT_PLANT,
+  ADD_PLANT,
+  DELETE_PLANT,
+} from "../actions/plantActions";
 
 export const initialState = {
-  plant: {
-    id: "",
-    nickname: "",
-    species: "",
-    h2oFrequency: "",
-    image: "",
-  },
+  plants: [],
   editing: false,
   isFetching: false,
 };
+// plants structure
+// plant: {
+//     id: "",
+//     nickname: "",
+//     species: "",
+//     h2oFrequency: "",
+//     image: "",
+//   }
 
 export const plantReducer = (state = initialState, action) => {
   switch (action.type) {
+    case LOADING_PLANTS: {
+      return {
+        ...state,
+        isLoading: true,
+        error: "",
+      };
+    }
+
+    case PLANT_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        plants: [...action.payload],
+      };
+    }
+
+    case PLANT_FAIL: {
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+    }
+
+    case ADD_PLANT: {
+      return {
+        ...state,
+        isLoading: false,
+        plants: action.payload,
+      };
+    }
+
+    case EDIT_PLANT: {
+      return {
+        ...state,
+        isLoading: false,
+        plants: action.payload,
+      };
+    }
+
+    case DELETE_PLANT: {
+      return {
+        ...state,
+        isLoading: false,
+        plants: action.payload,
+      };
+    }
+
     default:
       return state;
   }
