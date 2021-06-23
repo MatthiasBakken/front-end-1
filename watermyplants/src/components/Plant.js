c6a0c7e602597578f670aa415c131b0ce2d144d6
Matthias Bakken <36183902+MatthiasBakken@users.noreply.github.com>
2021-06-23 03:20:20 -0700

    added error messages to plants

--- watermyplants/src/components/Plant.js
+++ watermyplants/src/components/Plant.js
@@ -4,19 +4,20 @@ import { addPlant } from "../actions/plantActions";
 import { useHistory } from "react-router-dom";
 
 
-const Plant = (props) => {
-  const [values, setValues] = useState({
-    nickname: "",
-    species: "",
-    h20_frequency: "",
-    plant_img: "",
-  });
+const initialState = {
+  nickname: "",
+  species: "",
+  h20_frequency: "",
+  plant_img: "",
+};
 
-  const [errors, setErrors] = useState({
-    nickname: "",
-    species: "",
-    h20_frequency: "",
-  });
+const errors = {
+  length: "Must be at least 3 characters",
+  waterFrequency: "Must be more than zero"
+}
+
+const Plant = (props) => {
+  const [values, setValues] = useState(initialState);
 
   const addPlant = props.addPlant;
   const { push } = useHistory();
@@ -24,14 +25,6 @@ const Plant = (props) => {
   const onCancel = (evt) => {
     evt.preventDefault();
     push("/plantlist");
-    // reset()
-  };
-
-  const onSubmit = (evt) => {
-    evt.preventDefault();
-    addPlant(values);
-    push("/plantlist");
-    // submit()
   };
 
   const onChange = (evt) => {
@@ -41,7 +34,11 @@ const Plant = (props) => {
     });
   };
   return (
-    <form className="formContainer" onSubmit={onSubmit}>
+    <form className="formContainer" onSubmit={( evt ) => {
+      evt.preventDefault();
+      addPlant(values);
+      push("/plantlist");
+    }}>
       <h2>Water My Plants</h2>
 
       <div className="form-group errors">
@@ -51,40 +48,26 @@ const Plant = (props) => {
       </div>
 
       <div className="form-group inputs">
-        <h4>Add a plant </h4>
-        <label>
-          plant's nickname
-          <input value={values.nickname} onChange={onChange} name="nickname" />
-        </label>
+        <h4>Add a Plant </h4>
+        <label>Plant's Nickname:</label>
+        <input value={values.nickname} onChange={onChange} name="nickname" />
+        {values.nickname && values.nickname.length <= 3 ? <div>{errors.length}</div> : null}
+      
+        <label>Species:</label>
+        <input value={values.species} onChange={onChange} name="species" />
+        {values.species && values.species.length <= 3 ? <div>{errors.length}</div> : null}
 
-        <label>
-          species
-          <input value={values.species} onChange={onChange} name="species" />
-        </label>
-
-        <label>
-          watering frequency
-          <input
-            value={values.h20_frequency}
-            onChange={onChange}
-            name="h20_frequency"
-          />
-        </label>
-
-        <label>
-          <h6> - optional - </h6>
-          image URL:
-          <input
-            value={values.plant_img}
-            onChange={onChange}
-            name="plant_img"
-          />
-        </label>
+        <label>Watering Frequency:</label>
+        <input value={values.h20_frequency} onChange={onChange} name="h20_frequency" type="number" />
+        {values.h20_frequency && values.h20_frequency < 1 ? <div>{errors.waterFrequency}</div> : null}
+        
+        <label>Image URL:</label>
+        <input value={values.plant_img} onChange={onChange} name="plant_img" type="url"/>
       </div>
 
-      <div className="form-group submit">
+      <div className="form-group buttons">
         <button type="submit">add plant</button>
-        <button id="cancelBtn" onClick={onCancel}>
+        <button onClick={onCancel}>
           cancel
         </button>
       </div>
